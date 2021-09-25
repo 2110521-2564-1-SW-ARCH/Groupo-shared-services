65e49300ec4bbe021c7db4d78b9416c584c57b25
wongtawan-j <work@wongtawan.dev>
2021-09-26 03:33:28 +0700

    service interface

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
-export * from "./services";
-export * from "./apiutils";
+import ServicesInternal from "./services";
+import APIUtilsInternal from "./apiutils";
+
+export const Services = ServicesInternal;
+export const APIUtils = APIUtilsInternal;
