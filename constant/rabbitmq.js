d57078331908d188ad93a1976d36e21043ff7d7d
wongtawan-j <work@wongtawan.dev>
2021-10-26 19:09:16 +0700

    update services

---
+++ constant/rabbitmq.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.RabbitMQQueue = void 0;
+exports.RabbitMQQueue = "logging";
+//# sourceMappingURL=rabbitmq.js.map
